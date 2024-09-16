d614c17894f1a0b2586edd8ddb2f7b83b75670e7
enteresanlikk <zeki1198@gmail.com>
2024-09-17 01:08:36 +0300

    Add infrastructure layer.

---
+++ src/Bookify.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal abstract class Repository<T>
+    where T : Entity
+{
+    protected readonly ApplicationDbContext DbDontext;
+
+    protected Repository(ApplicationDbContext dbDontext)
+    {
+        DbDontext = dbDontext;
+    }
+
+    public async Task<T?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        return await DbDontext
+            .Set<T>()
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+
+    public void Add(T entity)
+    {
+        DbDontext.Add(entity);
+    }
+}
